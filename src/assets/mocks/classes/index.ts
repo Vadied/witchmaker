89e4b67c5c8b808d4a35e786b18ed9c203f96c2a
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-03-08 14:11:32 +0100

    add classes db

---
+++ src/assets/mocks/classes/index.ts
@@ -0,0 +1,33 @@
+import * as artificer from "./artificer.json";
+import * as barbarian from "./barbarian.json";
+import * as bard from "./bard.json";
+import * as cleric from "./cleric.json";
+import * as druid from "./druid.json";
+import * as fighter from "./fighter.json";
+import * as monk from "./monk.json";
+import * as mystic from "./mystic.json";
+import * as paladin from "./paladin.json";
+import * as ranger from "./ranger.json";
+import * as rogue from "./rogue.json";
+import * as runescribe from "./runescribe.json";
+import * as sorcerer from "./sorcerer.json";
+import * as warlock from "./warlock.json";
+import * as wizard from "./wizard.json";
+
+export default {
+  barbarian,
+  bard,
+  cleric,
+  druid,
+  fighter,
+  artificer,
+  monk,
+  mystic,
+  paladin,
+  ranger,
+  rogue,
+  runescribe,
+  sorcerer,
+  warlock,
+  wizard,
+};
